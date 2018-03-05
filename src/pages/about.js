18da54fbf51877ced55a5c1ef83346607e1169cd
Bob Walsh <bob.walsh@47hats.com>
2018-03-04 16:55:54 -0800

    up to lesson #8

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About cats</h1>
+        <p>stuff</p>
+      </div>
+    );
+  }
+}
+
+export default About;
